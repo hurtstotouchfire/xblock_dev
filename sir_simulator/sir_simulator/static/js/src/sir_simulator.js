a30b3102f100b4ca35c69ab07a4eee40041a73b6
Kelly <hurtstotouchfire@gmail.com>
2016-08-13 15:45:55 -0400

    add revised simulation and graph code

--- sir_simulator/sir_simulator/static/js/src/sir_simulator.js
+++ sir_simulator/sir_simulator/static/js/src/sir_simulator.js
@@ -26,83 +26,86 @@ function SIRSimulatorXBlock(runtime, element) {
     });
 
     $(function ($) {
-        
-        
-      // go get highcharts? or use addjavascript or whatever.  
-        
-        
-        function configureChart(timeSeries) {
-  $('#js-sir-simulaton-graph').highcharts({
-    chart: {
-      type: 'column'
-    },
-    title: {
-      text: ''
-    },
-    subtitle: {
-      text: ''
-    },
-    yAxis: {
-      min: 0,
-      title: {
-        text: 'Infected over time'
-      },
-      labels: {
-        formatter: function() {
-          return this.value + '%';
-        }
-      }
-    },
-    legend: {
-      enabled: false
-    },
-    tooltip: {
-      pointFormat: "Total infected: <b>{point.I}</b>"
-    },
-    series: [{
-      name: 'Infected',
-      data: timeSeries,
-      dataLabels: {
-        enabled: true,
-        rotation: 0,
-        color: '#FFFFFF',
-        align: 'right',
-        format: '{point.y:.0f}% ', // the trailing space is padding. Forgive me.
-        x: 0
-      }
-    }]
-  });
-};
 
-function runSimulation(N,b) {
-      var max_t = 20;
-      var I = 1;
-      var S = N-1;
-      var timeSeries = [{t:0, I:I}];
-      for (t = 1; t < max_t; t++) {
-          var p_inf = 1.0-Math.exp(-b*I/N);
-          var new_I = 0;
-          for (i = 0; i < S; i++) {
-              if (Math.random() < p_inf) {
-                  new_I++;
-              }
-          }
-          if (new_I == 0) {
-              break;
-          }
-          incidence.push({t:t, I:new_I});
-          S -= new_I;
-          I = new_I;
-      }
-      return(timeSeries);
-  }
-
-// actually do the things
-  timeSeries = runSimulation(50, 1.5);
-  configureChart(timeSeries);
-        
-        
-        
-        
+	function configureChart(timeSeries, xMax, yMax) {
+    $('#js-sir-simulaton-graph').highcharts({
+        chart: {
+            type: 'column'
+        },
+        title: {
+            text: ''
+        },
+        xAxis: {
+            min: 0,
+            max: xMax
+        },
+        yAxis: {
+            min: 0,
+            max: yMax,
+            title: {
+                text: 'Infected population',
+                align: 'high'
+            },
+            labels: {
+                overflow: 'justify'
+            }
+        },
+        plotOptions: {
+            bar: {
+                dataLabels: {
+                    enabled: true
+                }
+            }
+        },
+        credits: {
+            enabled: false
+        },
+        series: [{
+            name: 'Infections over time',
+            data: timeSeries
+        }]
     });
+    }
+    
+    function runSimulation(population, rNum, callback){
+      // simulation parameters
+    	var max_t = population;
+    	var I = 1; // initial infected person
+    	var S = population - 1; // initial susceptible population
+    	var timeSeries = [{t:0, y:I}]; // initial time point
+      
+      // simulation helpers
+      function infectionProbability(currentlyInfected) {
+      	return 1.0 - Math.exp(-rNum*currentlyInfected/population);
+      }
+      
+      function newInfectionsPerTimePoint(p_inf) {
+        var new_I = 0;
+    	  for (i = 0; i < S; i++) {
+          if (Math.random() < p_inf) {new_I++;}
+    	  }
+        return new_I;
+      }
+      
+      // run simulation 
+    	for (t = 1; t < max_t; t++) {
+    	  var p_inf = infectionProbability(I);
+    	  var newlyInfected = newInfectionsPerTimePoint(p_inf);
+    	  if (newlyInfected == 0) {
+          // stop creating timepoints when no one is infected
+          break;
+        };
+    	  timeSeries.push({t:t, y:newlyInfected});
+        // remove newly infected from the susceptible population
+    	  S -= newlyInfected;
+        // update infected count for next time step
+    	  I = newlyInfected;
+    	}
+      
+      callback(timeSeries, 0.7 * population, 0.7 * population);
+    };
+
+   runSimulation(10, 4, configureChart);
+   
+});
 }
