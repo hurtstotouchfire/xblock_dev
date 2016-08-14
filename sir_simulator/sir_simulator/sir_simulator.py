524db1cebb730eda3a1a60f9057dd1549ff6b1cf
hurtstotouchfire <hurtstotouchfire@gmail.com>
2016-08-14 18:44:19 -0400

    use StudioEditableXBlockMixin for studio view.

--- sir_simulator/sir_simulator/sir_simulator.py
+++ sir_simulator/sir_simulator/sir_simulator.py
@@ -5,15 +5,24 @@ import pkg_resources
 from xblock.core import XBlock
 from xblock.fields import Scope, Integer, Float, String
 from xblock.fragment import Fragment
+from xblockutils.studio_editable import StudioEditableXBlockMixin
 
-class SIRSimulatorXBlock(XBlock):
+class SIRSimulatorXBlock(StudioEditableXBlockMixin, XBlock):
     """
     Allows students to select parameters for an SIR model and describe the
     resulting simulation.
     """
 
-    # Learners are shown the max infected percent in each of their simulation
-    # runs. This number is a common metric for how debilitating an epidemic is
+    ############################################################################
+    # Fields
+    ############################################################################
+
+    editable_fields = ('preamble', 'directions')
+
+    # Learner-generated fields
+    ##########################
+
+    # This number is a common metric for how debilitating an epidemic is
     # so the student whose simulation produced the max_percent_infected would
     # have created the "worst" epidemic.
     max_percent_infected = Integer(
@@ -37,7 +46,46 @@ class SIRSimulatorXBlock(XBlock):
         help="A description of this outbreak simulation.",
     )
 
-    # TO-DO: change this view to display your data your own way.
+    # Instructor-configurable fields
+    ################################
+
+    preamble = String(# TODO: broaden this to include equations. Possibly expose
+        # more technical details to instructor in studio view only.
+        help="An explanation of what the SIR model is and directions for "\
+        "interacting with the simulation. We've included the limitations of "\
+        "the model here, but you may add to or reword the description.",
+        default="The SIR (Susceptible, Infected, Recovered) model is a "\
+        "simple dynamic system used to simulate an epidemic. In this "\
+        "simulation, a single person becomes infected with a virus. The "\
+        "infection lasts for one time step of the model, during which they "\
+        "may infect others in the population. Once infected and recovered, "\
+        "an individual is immune. The plague lasts until it dies out or all "\
+        "individuals in the population are immune. However, at any point in "\
+        "the plague, if over 10% of the population is incapacitated by the "\
+        "infection, basic civic infrastructure may begin to suffer.",
+        scope=Scope.user_state_summary,
+        display_name='Activity preamble',
+        multiline_editor=True
+    )
+
+    directions = String(
+        help="An explanation of what the learner should describe in their "\
+        "submission.",
+        default='Pick the parameters which you think represent the "worst '\
+        'case scenario". Then explain the features of the simulation which '\
+        'you think justify this.', 
+        scope=Scope.user_state_summary,
+        display_name='Directions',
+        multiline_editor=True
+    )
+
+    ############################################################################
+    # Views
+    ############################################################################
+
+    # studio_view inherited from StudioEditableXBlockMixin
+
+    # Displays a form for entering simulation parameters and runs the simulation
     def student_view(self, context=None):
         """
         The primary view of the SIRSimulatorXBlock, shown to students
