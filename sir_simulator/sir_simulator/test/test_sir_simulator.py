4dae7ad408eeee2ef5724e4138999f37de4598b4
hurtstotouchfire <hurtstotouchfire@gmail.com>
2016-08-17 21:17:05 -0400

    a few more unit tests

--- sir_simulator/sir_simulator/test/test_sir_simulator.py
+++ sir_simulator/sir_simulator/test/test_sir_simulator.py
@@ -26,7 +26,25 @@ def make_block():
     scope_ids = ScopeIds('user', block_type, def_id, usage_id)
     return SIRSimulatorXBlock(runtime, field_data, scope_ids=scope_ids)
 
-def test_default_values_of_fields():
+def test_default_values_of_model_parameters():
     test_xblock = make_block()
-        
+
     assert_equals(test_xblock.population, 50)
+    assert_equals(test_xblock.reproduction_num, 1.2)
+    assert_equals(test_xblock.max_percent_infected, 0)
+    # TODO: Figure out why this field isn't there.
+    # Maybe because this is the String default?
+    #assert_equals(test_xblock.simulation_description, '')
+
+def test_instructor_editable_fields():
+    test_xblock = make_block()
+
+    expected_editable_fields = ('preamble', 'directions')
+    assert_equals(test_xblock.editable_fields, expected_editable_fields)
+
+    for field in expected_editable_fields:
+        # field defaults should be more than some trivial length
+        assert(len(getattr(test_xblock, field)) > 5)
+
+        # field help text is required by StudioEditableXBlockMixin
+        assert(len(test_xblock.fields[field].help) > 5)
